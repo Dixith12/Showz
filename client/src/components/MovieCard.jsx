c2735157ecd530703c3204ff8d34379beabc2926
Deekshith <deekshithskulal485@gmail.com>
2025-10-27 19:37:23 +0530

    creation of Featured Section

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -14,7 +14,7 @@ const navigate = useNavigate();
       <p className='text-sm text-gray-400 mt-2'>{new Date(movie.release_date).getFullYear()} . {movie.genres.slice(0,2).map(genre=>genre.name).join(" | ")} . {timeFormat(movie.runtime)}</p>
       
       <div className='flex items-center justify-between mt-4 pb-3 '>
-        <button onClick={()=>{navigate('/movies/${movies._id}');scrollTo(0,0)}} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull trnsition rounded-full font-medium cursor-pointer'>
+        <button onClick={()=>{navigate(`/movies/${movie._id}`);scrollTo(0,0)}} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull trnsition rounded-full font-medium cursor-pointer'>
             Buy Tickets
         </button>
         <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
